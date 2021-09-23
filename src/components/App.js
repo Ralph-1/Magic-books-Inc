a278af02bde45fbca32a5ad5e96504aee4413fba
Ralph <ralphoburu@gmail.com>
2021-09-24 00:04:09 +0300

    Add booklist

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import '../App.css';
+import Bookslist from '../containers/BooksList';
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <Bookslist />
     </div>
   );
 }
---
+++ src/containers/BooksList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Bookslist() {
+  return (
+    <div>
+      <table>
+        <th>BookID</th>
+        <th>Title</th>
+        <th>Category</th>
+      </table>
+    </div>
+  );
+}
