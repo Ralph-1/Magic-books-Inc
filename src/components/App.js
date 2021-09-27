9b6c65b357eb3e5f29c7a4e2febc7c577cb56f9c
Ralph <ralphoburu@gmail.com>
2021-09-28 02:13:21 +0300

    Add navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import '../App.css';
 import BooksList from '../containers/BooksList';
 import BooksForm from '../containers/BooksForm';
+import Navbar from './NavBar';
 
 const App = () => (
   <div className="App">
+    <Navbar />
     <BooksForm />
     <BooksList />
   </div>
---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => (
+  <div className="nav-bar">
+    <h1>BookStore CMS</h1>
+    <p className="nav-books">BOOKS</p>
+    <p className="nav-cat">CATEGORIES</p>
+  </div>
+);
+
+export default Navbar;
